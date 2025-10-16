4d997d757e33ee057e7a243ece4750bc6c455a00
NicolaPiras97 <npiras1997@gmail.com>
2025-10-16 10:39:27 +0200

    Update Multilevel_CCLC_bayesian.cpp

--- Multilevel_CCLC_bayesian.cpp
+++ Multilevel_CCLC_bayesian.cpp
@@ -1,1 +1,48 @@
+// [[Rcpp::depends(BH)]]
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(RcppEigen)]]
+#include <iostream>
+#include </opt/homebrew/Cellar/boost/1.81.0_1/include/boost/random.hpp>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <armadillo>
+#include</opt/homebrew/Cellar/eigen/3.4.0_1/include/eigen3/unsupported/Eigen/CXX11/Tensor>
+#define _USE_MATH_DEFINES
+#include <chrono>
+
+
+using namespace Eigen;
+using namespace std;
+using namespace arma;
+
+
+tuple <mat,int,int> vector_to_matrix(const std::vector< std::vector<int> > &student_vector)
+{
+  mat y(student_vector.size(),student_vector[0].size());
+  
+  for(int i = 0; i < student_vector.size(); i++)
+  {
+    for(int j = 0; j < student_vector[i].size(); j++)
+    {
+      y(i,j) = student_vector[i][j];
+    }
+  }
+  
+  int _n=student_vector.size();
+  int _G=student_vector[0].size();
+  
+  return {y,_n,_G};
+}
+
+
+// [[Rcpp::export]]
+int main2(vector<vector<int>> yy){
+  mat y;
+  int _n;
+  int _G;
+  tie(y,_n,_G)=vector_to_matrix(yy);
+  
+    auto start = chrono::steady_clock::now();
+    std::vector< std::vector<int> > student_vector;
 
